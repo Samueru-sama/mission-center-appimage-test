afb7df1a8559074a24de0a400ecfa554f257d8dd
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-21 16:55:16 -0400

    oops

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,8 +14,8 @@ export DESKTOP=/usr/share/applications/io.missioncenter.MissionCenter.desktop
 # Deploy dependencies
 quick-sharun /usr/bin/missioncenter*
 
-echo 'MC_RESOURCE_DIR=${SHARUN_DIR}/share/missioncenter' >> ./AppDir/.env
-echo 'MC_MAGPIE_HW_DB=${SHARUN_DIR}/share/hw.db'         >> ./AppDir/.env
+echo 'MC_RESOURCE_DIR=${SHARUN_DIR}/share/missioncenter'       >> ./AppDir/.env
+echo 'MC_MAGPIE_HW_DB=${SHARUN_DIR}/share/missioncenter/hw.db' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
